547b456c42b6736b12980d11018e5876227de7fe
Shameer Shahzad <shameershazad156@gmail.com>
2026-05-14 19:41:20 +0500

    backend updated with vercel config and mongodb env

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,30 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-import Todo from "./models/Todo.js";
 import dotenv from "dotenv";
-import process from "process";
+import Todo from "./models/Todo.js";
+
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-dotenv.config();
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
-
-// Get Todos
 app.get("/todos", async (req, res) => {
   const todos = await Todo.find();
   res.json(todos);
 });
 
+app.get("/", (req, res) => {
+  res.send("Backend Running");
+});
 
-// Add Todo
 app.post("/todos", async (req, res) => {
   const newTodo = new Todo({
     text: req.body.text,
@@ -35,7 +35,4 @@ app.post("/todos", async (req, res) => {
   res.json(newTodo);
 });
 
-
-app.listen(5000, () => {
-  console.log("Server Running on Port 5000");
-});
+export default app;
